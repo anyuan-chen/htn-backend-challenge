c2855a2e910a30fcb514b7853b130f189760124e
Andrew Chen <andrew.chen.anyuan@gmail.com>
2023-02-16 15:43:48 -0500

    bug: failed update resolver

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,5 +1,10 @@
+import { Skill } from "@prisma/client";
 import { context } from "./context.js";
-import { UserCreateInput } from "./types.js";
+import { UserCreateInput, UserUpdateInput } from "./types.js";
+
+const allUserResolver = (parent, args, contextValue) => {
+  return context.prisma.user.findMany();
+};
 
 const userResolver = (parent, args, contextValue) => {
   return context.prisma.user.findFirst({
@@ -13,7 +18,10 @@ const userResolver = (parent, args, contextValue) => {
     },
   });
 };
-const skillFrequencyResolver = (parent, args, contextValue) => {};
+
+const skillFrequencyResolver = (parent, args, contextValue) => {
+  return context.prisma.skill.count;
+};
 
 const skillsResolver = (parent, args, contextValue) => {
   return context.prisma.skill.findMany();
@@ -48,10 +56,74 @@ const userCreateInputResolver = async (
     },
   });
 };
+const updateUserResolver = async (
+  parent,
+  args: { data: UserUpdateInput; uid: number },
+  contextValue
+) => {
+  if (args.data.skills) {
+    //can't do upsert many here which would be more efficient
+
+    //Two options:
+    //Option 1 (implemented)
+    //read all the skills which already exist for the users
+    //filter to two lists:
+    //update all skills that already exist
+    //create new skills that doen't exist
+
+    const all_skills = await context.prisma.skill.findMany({
+      where: {
+        user_uid: args.uid,
+      },
+    });
+    let update_list: Skill[] = [];
+    let create_list: Skill[] = [];
+    for (const skill of all_skills) {
+      if (
+        all_skills.find(
+          (unknown_skill) => skill.skill == unknown_skill.skill
+        ) != undefined
+      ) {
+        update_list.push(skill);
+      } else {
+        create_list.push(skill);
+      }
+    }
+    update_list.forEach((skill) => {
+      context.prisma.skill.update({
+        where: {
+          uid: skill.uid,
+        },
+        data: {
+          skill: skill.skill,
+          rating: skill.rating,
+        },
+      });
+    });
+    create_list.forEach((skill) => {
+      context.prisma.skill.create({
+        data: skill,
+      });
+    });
+  }
+  return context.prisma.user.update({
+    where : {
+        uid: args.uid
+    }, 
+    data: {
+        name: args.data.name,
+        company: args.data.company,
+        phone: args.data.phone,
+        email: args.data.email,
+    }
+  })
+};
 
 export {
   userResolver,
   skillFrequencyResolver,
   skillsResolver,
   userCreateInputResolver,
+  allUserResolver,
+  updateUserResolver,
 };
